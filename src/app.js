5412fac6765823c6805723fcc189349cba3016c8
Carlos A Palacios R <carlosargelio0104@gmail.com>
2024-03-13 15:38:05 -0400

    feat: add endpoint welcome

--- src/app.js
+++ src/app.js
@@ -30,18 +30,49 @@ const createApp = () => {
 
   // middlewares
   app.use(cors(whiteList.cors))
-  app.use(function(req, res, next) {
-		IpFilter(whiteList.ips, {mode: "allow", log: false, detectIp: clientIp})(req, res, function(err) {
-			if (err === undefined) {
-				return next();
-			}
-			res.status(403).json({
-        message: 'Forbidden',
-        code: 403,
-        error: "You are not authorized to access this page.",
-      });
-		});
-	});
+
+  // Development | is used to validated IPs Address
+  // app.use(function(req, res, next) {
+	// 	IpFilter(whiteList.ips, {mode: "allow", log: false, detectIp: clientIp})(req, res, function(err) {
+	// 		if (err === undefined) {
+	// 			return next();
+	// 		}
+	// 		res.status(403).json({
+  //       message: 'Forbidden',
+  //       code: 403,
+  //       error: "You are not authorized to access this page.",
+  //     });
+	// 	});
+	// });
+
+  app.get('/', (req, res) => {
+    const htmlResp = `
+    <html>
+    <head>
+      <title>
+        NodeJs and Express on Vercel
+      </title>
+    </head>
+    <body>
+      Iam project backend on version 'v1'
+    </body>
+
+    <section>
+      <p>Integrants:</p>
+
+      <ul>
+        <li>
+          Pedro Montes
+        </li>
+        <li>
+          Carlos Palacios
+        </li>
+      </ul>
+    </section>
+  </html>
+    `
+    res.send(htmlResp)
+  })
 
   app.use('/api/v1', router)
 
