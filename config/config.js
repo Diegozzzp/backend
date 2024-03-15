d8b36293fc2e3060bdee7844b7cf7361912f4f94
Carlos A Palacios R <carlosargelio0104@gmail.com>
2024-03-14 22:10:24 -0400

    fix

--- config/config.js
+++ config/config.js
@@ -1,8 +1,15 @@
 import 'dotenv/config'
 
+let CORS_WHITELIST = null
+if (!process.env.CORS_WHITELIST) {
+  CORS_WHITELIST = [['190.0.0.0', '190.255.255.255'],['200.0.0.0', '200.255.255.255']]
+} else {
+  CORS_WHITELIST = btoa(process.env.CORS_WHITELIST.split(','))
+}
+
 const config = {
   whiteList: {
-    corsAllow: btoa(process.env.CORS_WHITELIST.split(',')),
+    corsAllow: CORS_WHITELIST,
     ipsAllow: JSON.parse(process.env.IPS_ALLOW.replace(/'/g, '"'))
   }
 }
